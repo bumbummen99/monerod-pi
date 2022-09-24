cdd5bfe20a8470bb9b0e4253f91ebffbe53bf0d8
Patrick <privat@skyraptor.eu>
2022-09-24 20:23:35 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@
 wget https://downloads.getmonero.org/cli/linuxarm8 -O monero-cli.tar.bz2
 
 # Extract downloaded monero client
-mkdir -p $HOME/software
-tar -xf monero-cli.tar.bz2 --strip 1 -C $HOME/software
+mkdir -p $1/software
+tar -xf monero-cli.tar.bz2 --strip 1 -C $1/software
 
 # Cleanup
 rm -f monero-cli.tar.bz2
